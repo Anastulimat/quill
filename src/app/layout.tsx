f492e0d373ea5c4d8c0d6b313bbff27a8f47bb5c
TULIMAT Anas OBS/GDO <TZKM2460@orange.com>
2023-09-30 23:40:22 +0200

    feat!: install kinde auth and add navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
 import {ReactNode} from "react";
 import {cn} from "@/lib/utils";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({subsets: ['latin']})
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     return (
         <html lang="en" className="light">
             <body className={cn('min-h-screen font-sans antialiased grainy', inter.className)}>
+                <Navbar />
                 {children}
             </body>
         </html>
